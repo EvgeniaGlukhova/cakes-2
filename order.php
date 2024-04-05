fbcae48f8ccebf3c1a069aa62351db846b32df9a
EvgeniaGlukhova <125294562+EvgeniaGlukhova@users.noreply.github.com>
2024-04-05 13:16:06 +0500

    Update order.php

--- order.php
+++ order.php
@@ -9,17 +9,26 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include_once 'header.php' ?>
+    <?php include_once 'header.php'?>
 
+    <?php
+    $namef = array_key_exists("fName", $_COOKIE) ? $_COOKIE['fName'] : "";
+    $namel = array_key_exists("lName", $_COOKIE) ? $_COOKIE['lName'] : "";
+    $nameE = array_key_exists("Email", $_COOKIE) ? $_COOKIE['Email'] : "";
+
+
+?>
+
+    
 
 
     <main class="main">
         <div class="main-container">
             <h2 class="order">Оформление заказа</h2><br>
             <form action="user.php" method="POST" enctype="multipart/form-data" >
-            <input class="login-input" type="text" name="lName" id="lName" placeholder="Фамилия" require><br>
-                <input class="login-input" type="text" name="fName" id="fName" placeholder="Имя" require><br>
-                <input class="login-input" type="text" name="Email" id="Email" placeholder="Почта" require><br>
+            <input class="login-input" type="text" name="lName" id="lName" placeholder="Фамилия"  value="<?=$namef?>"><br>
+                <input class="login-input" type="text" name="fName" id="fName" placeholder="Имя" value="<?=$namel?>"><br>
+                <input class="login-input" type="text" name="Email" id="Email" placeholder="Почта" value="<?=$nameE?>"><br>
                 <select class="login-input" name="Topping" id="Topping" placeholder="Начинка">
                     <option value="0">Шоколадное трио
                     <option value="1">Флорида
