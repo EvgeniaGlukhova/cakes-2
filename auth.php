6a56fc281bf94d267bd679aee5aca56dff25d253
EvgeniaGlukhova <125294562+EvgeniaGlukhova@users.noreply.github.com>
2024-04-26 12:17:44 +0500

    аутентификация с использованием базы данных

--- auth.php
+++ auth.php
@@ -1,10 +1,18 @@
 <?php
     session_start(["use_strict_mode" => true]);
+   
     unset($_SESSION['message']);
+
     if (isset($_POST['login'])){
-        if ($_POST['login'] == 'cakes'){
-            if ($_POST['password'] == '2004'){
-                $_SESSION['username'] = $_POST['login'];
+
+        require_once 'dbconnect.php';
+
+        $result = $db->query("SELECT * FROM cakes.\"Пользователь\" WHERE Логин = '".$_POST['login']."'");
+
+        if ($row = $result->fetch())
+        {
+            if (MD5($_POST['password']) == $row['Пароль']){
+                $_SESSION['username'] = $row['Логин'];
                 header("Location: login.php");
                 die();
             }
@@ -12,7 +20,7 @@
                 $_SESSION['error'] = 'Вы ввели неправильный пароль!';
                 header("Location: login.php");
                 die();
-            } 
+            }
 
         }
         else {
