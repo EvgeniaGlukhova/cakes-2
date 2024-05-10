2d57ccfb5903aab636f8d01af4aaad5196449869
EvgeniaGlukhova <125294562+EvgeniaGlukhova@users.noreply.github.com>
2024-05-10 12:14:40 +0500

    добавление регистраций

--- auth.php
+++ auth.php
@@ -1,38 +1,71 @@
 <?php
-    session_start(["use_strict_mode" => true]);
-   
-    unset($_SESSION['message']);
 
-    if (isset($_POST['login'])){
+session_start(["use_strict_mode" => true]);
 
-        require_once 'dbconnect.php';
+if (isset($_GET['log'])) {
+    require_once 'dbconnect.php';
 
-        $result = $db->query("SELECT * FROM cakes.\"Пользователь\" WHERE Логин = '".$_POST['login']."'");
+    $query = $db->query("SELECT * FROM cakes.\"Пользователь\" WHERE Логин = '".$_POST['login']."'");
 
-        if ($row = $result->fetch())
-        {
-            if (MD5($_POST['password']) == $row['Пароль']){
-                $_SESSION['username'] = $row['Логин'];
-                header("Location: login.php");
-                die();
-            }
-            else {
-                $_SESSION['error'] = 'Вы ввели неправильный пароль!';
-                header("Location: login.php");
-                die();
-            }
+    if ($row = $query->fetch()) {
+        if ($row['Пароль'] == $_POST['password']) {
+            $_SESSION['client_id'] = $row['Клиенты'];
+            $_SESSION['login'] = $row['Логин'];
+            header("Location: profile.php");
+            die();
 
-        }
-        else {
-            $_SESSION['error'] = 'Вы ввели неправильный логин!';
+        } else {
+            $_SESSION['message'] = 'Введен неправильный пароль';
             header("Location: login.php");
             die();
         }
-
-    }
-     if ($_GET['logout'] == 1){
-       session_unset();
-        $_SESSION['exit'] = 'Вы успешно вышли из системы';
+        
+    } else {
+        $_SESSION['message'] = 'Введен неправильный логин'; 
         header("Location: login.php");
         die();
-     }
+    }
+
+    
+}
+
+if (isset($_GET['reg'])) {
+    require_once 'dbconnect.php';
+
+    $query = $db->query("SELECT * FROM cakes.\"Пользователь\" WHERE Логин = '".$_POST['login']."'");
+
+    if (isset($_FILES['file'])) {
+        $fp = fopen($_FILES['file']['tmp_name'], 'rb');
+        $bin_img = base64_encode(fread($fp, $_FILES['file']['size']));
+        fclose($fp);
+    } else {
+        $bin_img = 'NULL';
+    }
+
+    if ($row = $query->fetch()) {
+        $_SESSION['message'] = 'Такой пользователь уже существует';
+        header("Location: register.php");
+        die();
+    } else {
+        $client_id_query = $db->query("SELECT MAX(id_клиента) FROM cakes.\"Клиенты\"");
+        $client_id_row = $client_id_query->fetch();
+        $client_id = $client_id_row['max'] + 1;
+        $db->query("INSERT INTO cakes.\"Клиенты\"(id_клиента, \"Фамилия \", \"Имя\", \"Отчество \") VALUES (".$client_id.", '".$_POST['fName']."', '".$_POST['lName']."', '".$_POST['otch']."')");
+        $user_id_query = $db->query("SELECT MAX(\"id_пользователя\") FROM cakes.\"Пользователь\"");
+        $user_id_row = $user_id_query->fetch();
+        $user_id = $user_id_row['max'] + 1;
+        $db->query("INSERT INTO cakes.\"Пользователь\"(\"id_пользователя\", \"Логин\", \"Пароль\", \"Аватарка\", \"Клиенты\") VALUES (".$user_id.", '".$_POST['login']."', '".$_POST['password']."', '".$bin_img."', ".$client_id.")");
+        $_SESSION['client_id'] = $client_id;
+        $_SESSION['login'] = $_POST['login'];
+        header("Location: profile.php");
+        die();
+    }
+
+    
+}
+
+if (isset($_GET['out'])) {
+    session_unset();
+    header("Location: index.php");
+    die();
+}
